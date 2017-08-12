7e46c59e7bbaa3b698df866896486d4bb441f583
Zhechko Zhechev <jechkoj@gmail.com>
2017-08-13 00:03:06 +0200

    implements predecessor and successor for street segments

--- scripts/mmk_exp.py
+++ scripts/mmk_exp.py
@@ -11,13 +11,37 @@ import utm
 # get a CityEngine instance
 ce = CE()
 
-class Vertex():
+class Vertex(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+        
+class MMKGraphItem(object):
+    def __init__(self, itemUUID, osm_id):
+        self.UUID = itemUUID
+        self.osm_id = int(osm_id)
+        self.vertices = []
+        self.pred = []
+        self.succ = []
+        
+    def appendPred(pred):
+        self.pred.append(pred)
+    
+    def appendSucc(succ):
+        self.succ.append(succ)
+        
+    def appendVertex(self, x, y, z):
+        vertex = Vertex(x, y, z)
+        self.vertices.append(vertex)
+    
+    def __str__(self):
+        __repr__()
+        
+    def __repr__(self):
+        return str(self.osm_id) + ', ' + str(self.pred) + ', ' + str(self.succ)
 
-class CoordiantesConvertor():
+class CoordiantesConvertor(object):
     def __init__(self):
         (authAndCode, _) = ce.getSceneCoordSystem()
         
@@ -41,9 +65,66 @@ class CoordiantesConvertor():
         return utm.to_latlon(abs(easting), abs(northing), self.zoneNumber, northern=self.northern)
   
 def parseSegments(segments):
+    items = []
     for segment in segments:
-        osm_id = ce.getAttribute(segment, 'osm_id ')
+        if (ce.getAttribute(segment, 'highway') == 'residential' or
+            ce.getAttribute(segment, 'highway') == 'secondary' or
+            ce.getAttribute(segment, 'highway') == 'primary' or
+            ce.getAttribute(segment, 'highway') == 'motorway'):
+            
+            item = MMKGraphItem(ce.getOID(segment), ce.getAttribute(segment, 'osm_id'))
+            verticesList=ce.getVertices(segment)
+            
+            for i in xrange(0, (len(verticesList)-1),3):
+                item.appendVertex(verticesList[i], verticesList[i+1], verticesList[i+2]) 
+            
+            items.append(item)
+            
+    osm = ET.parse('C:\\Users\\Zhechev\\Documents\\IDP\\MMK\\scripts\\tum.osm')
+    wayEndpoints = {}
     
+    for item in items:
+        way = osm.find("./way[@id='" + str(item.osm_id) + "']")
+        if way == None:
+            print('Warning: ' + str(item.osm_id) + ' is not valid and will be omitted!')
+            continue
+
+        # Find all nodes in the current way
+        nodes = way.findall('nd')
+       
+        # OSM format guarantees 2 nodes per valid way element
+        firstNode = nodes[0].get('ref')
+        lastNode = nodes[-1].get('ref')
+        
+        # Collect the two endpoints
+        wayEndpoints[item.osm_id] = (firstNode, lastNode)
+    
+    for i in xrange(0, len(items)):
+        for j in xrange(i+1, len(items)):
+            #
+            if ((items[i].vertices[0].x == items[j].vertices[0].x and items[i].vertices[0].y == items[j].vertices[0].y and
+                items[i].vertices[0].z == items[j].vertices[0].z) or
+                (items[i].vertices[1].x == items[j].vertices[1].x and items[i].vertices[1].y == items[j].vertices[1].y and
+                items[i].vertices[1].z == items[j].vertices[1].z)):
+                
+                firstEndPoints = wayEndpoints.get(items[i].osm_id, None)
+                secondEndPoints = wayEndpoints.get(items[j].osm_id, None)
+                
+                if not firstEndPoints or not secondEndPoints:
+                    print('Error: No data about segment ' + str(items[i].osm_id) + ' or ' + str(items[j].osm_id))
+                    continue
+                
+                if wayEndpoints[items[i].osm_id][0] == wayEndpoints[items[j].osm_id][1]:
+                    items[i].appendPred(items[j].osm_id)
+                    items[j].appendSucc(items[i].osm_id)
+                elif wayEndpoints[items[i].osm_id][1] == wayEndpoints[items[j].osm_id][0]:
+                    items[i].appendSucc(items[j].osm_id)
+                    items[j].appendPred(items[i].osm_id)
+                else:
+                    print('Error: Segments are not neighbours in map world!')
+              
+    print(items)           
+   
 if __name__ == '__main__':
     cc = CoordiantesConvertor()
     
